26e7b01dc8559476546b867ce82db57b867e20b2
Olawole Olawale Martins <olawulzy@yahoo.co.uk>
2023-06-22 22:37:34 +0100

    factors edited

--- factors.c
+++ factors.c
@@ -1,41 +1,42 @@
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
 
-int main(int argc, char **argv)
+
+int main(int argc, char *argv[])
 {
-	char *line = 0;
-	size_t n = 0;
-	unsigned long long num;
-	unsigned long long i;
-	int solved;
 	FILE *file;
+	char *line = NULL;
+	size_t n = 0;
+	unsigned long long div, number;
+	int unsolved =  1;
+
+	if (argc != 2) {
+		fprintf(stderr, "Usage: %s <file>\n", argv[0]);
+		exit(EXIT_FAILURE);
+	}
 
 	file = fopen(argv[1], "r");
-	while(getline(&line, &n, file) != -1)
-	{
-		solved = 0;
-		num = atoll(line);
-		i = 2;
-		if (num % i == 0)
-		{
-			printf("%llu=%llu*%llu\n", num, num/i, i);
-			solved = 1;
-			continue;
-		}
-		i++;
-		while (i * i <= num)
-		{
-			if (num % i == 0)
-			{
-				printf("%llu=%llu*%llu\n", num, num/i, i);
-				solved = 1;
-				break;
-			}
-			i += 2;
-		}
-		if (!solved)
-			printf("%llu=%llu*%u\n", num, num, 1);
+	if (file == NULL) {
+		fprintf(stderr,"Error opening file\n");
+		exit(EXIT_FAILURE);
 	}
+
+	while (getline(&line, &n, file) != -1) {
+		unsolved = 1;
+		div = 2;
+		number = atoll(line);
+		while (unsolved && div * div <= number) {
+			if (number % div == 0) {
+				printf("%llu=%llu*%lld\n", number, number / div, div);
+				unsolved = 0;
+			}
+			div++;
+		}
+		if (unsolved)
+			printf("%llu=%llu*%d\n", number, number, 1);
+	}
+
 	free(line);
-	return 0;
+	fclose(file);
+	exit(EXIT_SUCCESS);
 }
--- test.c
+++
@@ -1,8 +0,0 @@
-#include <stdlib.h>
-#include <stdio.h>
-
-int main()
-{
-	unsigned long long num = atoll("1718944270642558716715");
-	printf("%llu", num);
-}
